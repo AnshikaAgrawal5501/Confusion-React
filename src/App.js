06dc7afd9cb23cc30082ed3b3bee014a8df0a693
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-09-01 00:10:31 +0530

    home page and navigation added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import Header from './Components/HeaderComponent';
 import Menu from './Components/MenuComponent';
 import Footer from './Components/FooterComponent';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './Components/HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
+
+  function HomePage() {
+    return <Home />;
+  }
+
+  function MenuPage() {
+    return <Menu />;
+  }
+
   return (
-      <div>
-        <Header />
-        <Menu />
-        <Footer />
-      </div>
+      <BrowserRouter>
+          <Header />
+          
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/menu' component={MenuPage} />
+              <Redirect to="/home" />
+          </Switch>
+            
+          <Footer />
+      </BrowserRouter>
     );
 }
 
--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+
+  const [isNavOpen, navFunc]=React.useState(false);
+
+  function toggleNav() {
+    navFunc(!isNavOpen);
+  }
+
   return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+          <Navbar dark expand="md">
+              <div className="container">
+                  <NavbarToggler onClick={toggleNav} />
+                  <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' /></NavbarBrand>
+              
+                  <Collapse isOpen={isNavOpen} navbar>
+                      <Nav navbar>
+                          <NavItem>
+                              <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                          </NavItem>
+                      </Nav>
+                  </Collapse>
+              </div>
+          </Navbar>
 
-        <Jumbotron>
-           <div className="container">
-               <div className="row row-header">
-                   <div className="col-12 col-sm-6">
-                       <h1>Ristorante con Fusion</h1>
-                       <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
-                   </div>
-               </div>
-           </div>
-       </Jumbotron>
+          <Jumbotron>
+              <div className="container">
+                  <div className="row row-header">
+                      <div className="col-12 col-sm-6">
+                          <h1>Ristorante con Fusion</h1>
+                          <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
+                      </div>
+                  </div>
+              </div>
+          </Jumbotron>
       </div>
     );
 }
